a13251f3f5b5b9c686c6614f769e209c44aed2fd
guydegnol <gt@guydegnol.net>
2022-10-20 10:57:35 +0200

    Add some changes

---
+++ exercices/example_td_1.py
@@ -0,0 +1,16 @@
+def function_1(num): 
+	""" 
+	function to print cube of given num 
+	"""
+	print("Cube: {}".format(num * num * num)) 
+
+def function_2(num): 
+	""" 
+	function to print square of given num 
+	"""
+	print("Square: {}".format(num * num)) 
+
+if __name__ == "__main__": 
+
+	# both processes finished 
+	print("Done!") 
