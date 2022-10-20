ab8ff2238a4c00fc06b044f4865463c6b734ab6f
guydegnol <gt@guydegnol.net>
2022-10-20 13:18:35 +0200

    Some change

--- hpcourse/__init__.py
+++ hpcourse/__init__.py
@@ -1,3 +1,5 @@
+__version__='0.1.5'
+
 #from .hpcourse import *
 #from .hpcourse3 import *
 
--- setup.py
+++ setup.py
@@ -1,5 +1,16 @@
 from distutils.core import setup
 
+
+def get_version():
+    for filename in [f"{get_parameter()}/{f}" for f in ["__version__.py", "__init__.py"]]:
+        if os.path.exists(filename):
+            for line in read(filename).splitlines():
+                if line.startswith("__version__"):
+                    delim = '"' if '"' in line else "'"
+                    return line.split(delim)[1]
+    raise RuntimeError("Unable to find version string.")
+
+
 setup(
     name='hpcgpu',
     version='0.1.5',
