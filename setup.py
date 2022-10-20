8975c29aee7164357b8b7e82160e6cdff4a47a61
guydegnol <gt@guydegnol.net>
2022-10-20 13:13:45 +0200

    Some change

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ setup(
     version='0.1.5',
     author='Guillaume Therin',
     author_email='guillaume@therin.co',
-    py_modules=['nvcc_plugin', 'v2.v2', 'v1.v1', 'common.helper', '__init__'],
+    py_modules=['nvcc_plugin', 'v2.v2', 'v1.v1', 'common.helper', 'hpcourse.hpcourse', 'hpcourse.hpcourse3'],
     url='https://github.com/guydegnol/hpcgpu_course',
     license='LICENSE',
-    description='to run CUDA C/C++ code',
+    description='Course on HPC with CUDA',
     #description='Jupyter notebook plugin to run CUDA C/C++ code',
     # long_description=open('README.md').read(),
 )
