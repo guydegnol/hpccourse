90232de41bf7ac2602aa452ef69647d3058024d1
guydegnol <gt@guydegnol.net>
2022-10-26 15:11:41 +0200

    play with plugin

--- hpcourse/evaluation.py
+++ hpcourse/evaluation.py
@@ -4,7 +4,7 @@ from firebase_admin import credentials
 from google.cloud import firestore
 
 
-def evaluate(cell, filename="hpcgpu_course/hpcourse/ipsastudents.json"):
+def evaluate(cell):
 
     # cred = credentials.Certificate(filename)
     # firebase_admin.initialize_app(cred)
@@ -35,43 +35,15 @@ class Evaluation(Magics):
             "-t", "--timeit", action="store_true", help="flag to return timeit result instead of stdout"
         )
 
-    @staticmethod
-    def compile(file_path):
-        subprocess.check_output(
-            [compiler, file_path + ".cu", "-o", file_path + ".out", "-Wno-deprecated-gpu-targets"],
-            stderr=subprocess.STDOUT,
-        )
-
-    def run(self, file_path, timeit=False):
-        if timeit:
-            stmt = f"subprocess.check_output(['{file_path}.out'], stderr=subprocess.STDOUT)"
-            output = self.shell.run_cell_magic(magic_name="timeit", line="-q -o import subprocess", cell=stmt)
-        else:
-            output = subprocess.check_output([file_path + ".out"], stderr=subprocess.STDOUT)
-            output = output.decode("utf8")
-
-        for l in output.split("\n"):
-            print(l)
-
-        return None
-
     @cell_magic
-    def nvcudac_and_exec(self, line, cell):
+    def evaluate_cell(self, line, cell):
         try:
             args = self.argparser.parse_args(line.split())
         except SystemExit as e:
             self.argparser.print_help()
             return
 
-        with tempfile.TemporaryDirectory() as tmp_dir:
-            file_path = os.path.join(tmp_dir, str(uuid.uuid4()))
-            with open(file_path + ".cu", "w") as f:
-                f.write(cell)
-            try:
-                self.compile(file_path)
-                output = self.run(file_path, timeit=args.timeit)
-            except subprocess.CalledProcessError as e:
-                for l in e.output.decode("utf8").split("\n"):
-                    print(l)
-                output = None
+        print(cell)
+
+        output = None
         return output
