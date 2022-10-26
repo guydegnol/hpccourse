04ff1ab61999a81b837f1c90cb648b4391dcf005
guydegnol <gt@guydegnol.net>
2022-10-26 13:43:54 +0200

    play with plugin

--- hpcourse/__init__.py
+++ hpcourse/__init__.py
@@ -1,7 +1,5 @@
 __version__ = "1.0.1"
 
-# from .hpcourse import *
-# from .hpcourse3 import *
 from .block import Block
 from .blockchain import BlockChain
 
@@ -11,7 +9,11 @@ def get_p():
 
 
 def load_extra_magics(ip):
+    # from .magics import NVCCUDACPlugin, NVCUDACPluginBis
     from .magics import NVCUDACPlugin, NVCUDACPluginBis
 
-    ip.register_magics(NVCCUDACPlugin(ip))
-    ip.register_magics(NVCUDACPluginBis(ip))
+    nvcc_plugin1 = NVCUDACPlugin(ip)
+    ip.register_magics(nvcc_plugin1)
+
+    nvcc_plugin2 = NVCUDACPluginBis(ip)
+    ip.register_magics(nvcc_plugin2)
