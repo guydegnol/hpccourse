83ba64ba538eec7f5947bad87b832e84abfad581
guydegnol <gt@guydegnol.net>
2022-10-26 20:46:28 +0200

    play with plugin

--- hpcourse/blockchain.py
+++ hpcourse/blockchain.py
@@ -35,7 +35,7 @@ class BlockChain:
 
     def construct_block(self, proof_no, prev_hash):
         # constructs a new block and adds it to the chain
-        block = hpcourse.Block(index=len(self.chain), proof_no=proof_no, prev_hash=prev_hash, data=self.current_data)
+        block = Block(index=len(self.chain), proof_no=proof_no, prev_hash=prev_hash, data=self.current_data)
         self.current_data = []
 
         self.chain.append(block)
