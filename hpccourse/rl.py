b02837e757594d698911e25e9c1b58aad3df4391
guydegnol <gt@guydegnol.net>
2022-12-22 14:43:31 +0100

    Tune rl stuffs

--- hpccourse/rl.py
+++ hpccourse/rl.py
@@ -1,22 +1,27 @@
 import matplotlib.pyplot as plt
-import subprocess
-import argparse
 import sys
 import numpy as np
 
-from IPython.core.magic import Magics, line_cell_magic, magics_class
+
+def runrealcmd(command):
+    from subprocess import Popen, PIPE, STDOUT
+
+    process = Popen(command, stdout=PIPE, shell=True, stderr=STDOUT, bufsize=1, close_fds=True)
+    for line in iter(process.stdout.readline, b""):
+        print(line.rstrip().decode("utf-8"))
+    process.stdout.close()
+    process.wait()
 
 
-# @register_line_magic
-# def init_env(args):
 def init_env(ip):
     """Use pip from the current kernel"""
     import tensorflow as tf
-    from pip import main
-    import matplotlib.pyplot as plt
 
-    ip.os.system("apt-get install -y xvfb python-opengl")
-    ip.os.system("pip install gymnasium pyvirtualdisplay gymnasium[atari,toy_text,box2d]")
+    runrealcmd("apt-get install -y xvfb python-opengl")
+    runrealcmd("pip install gymnasium pyvirtualdisplay array2gif")
+    runrealcmd("pip install gymnasium[atari,toy_text,box2d,classic_control,accept-rom-license]")
+    #%pip install gymnasium
+    #%pip install gymnasium[classic_control,box2d,atari,accept-rom-license]
 
     if not tf.config.list_physical_devices("GPU"):
         print("No GPU was detected. Neural nets can be very slow without a GPU.")
@@ -33,26 +38,6 @@ def init_env(ip):
     plt.rc("animation", html="jshtml")
 
 
-@magics_class
-class RLPlugin(Magics):
-    def __init__(self, shell):
-        super(RLPlugin, self).__init__(shell)
-        self.argparser = argparse.ArgumentParser(description="ipsa_compile_and_exec params")
-        self.argparser.add_argument(
-            "-t", "--timeit", action="store_true", help="flag to return timeit result instead of stdout"
-        )
-        self.argparser.add_argument("-c", "--compiler", default="nvcc", choices=["nvcc", "g++", "gcc"])
-
-    @line_cell_magic
-    def rl_init(self, line, cell="", local_ns=None):
-        try:
-            args = self.argparser.parse_args(line.split())
-        except SystemExit as e:
-            self.argparser.print_help()
-            return
-        subprocess.check_output("ls -1".split(), stderr=subprocess.STDOUT)
-
-
 def plot_environment(env, figsize=(5, 4)):
     plt.figure(figsize=figsize)
     img = env.render()
@@ -60,6 +45,7 @@ def plot_environment(env, figsize=(5, 4)):
         img = img[0]
     plt.imshow(img)
     plt.axis("off")
+    plt.show()
     return img
 
 
@@ -99,7 +85,7 @@ def show_one_episode(policy, n_max_steps=200, seed=42):
     frames = []
     env = gym.make("CartPole-v1", render_mode="rgb_array")
     np.random.seed(seed)
-    obs, info = env.reset(seed=seed)
+    obs, _ = env.reset(seed=seed)
     for step in range(n_max_steps):
         frames.append(env.render())
         action = policy(obs)
@@ -113,3 +99,17 @@ def show_one_episode(policy, n_max_steps=200, seed=42):
 def basic_policy(obs):
     angle = obs[2]
     return 0 if angle < 0 else 1
+
+
+def play_one_step(env, obs, model, loss_fn):
+    import tensorflow as tf
+
+    with tf.GradientTape() as tape:
+        left_proba = model(obs[np.newaxis])
+        action = tf.random.uniform([1, 1]) > left_proba
+        y_target = tf.constant([[1.0]]) - tf.cast(action, tf.float32)
+        loss = tf.reduce_mean(loss_fn(y_target, left_proba))
+
+    grads = tape.gradient(loss, model.trainable_variables)
+    obs, reward, done, truncated, info = env.step(int(action))
+    return obs, reward, done, truncated, grads
